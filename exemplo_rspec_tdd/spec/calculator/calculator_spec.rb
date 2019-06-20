155c0e42e2c20663bf06cf0ea51ef3040f908af6
Tiago Pereira <tiago.peng@gmail.com>
2019-06-20 18:29:29 -0300

    Estudo de matchers

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,23 +1,22 @@
-require 'calculator'
+require "calculator"
 
 describe Calculator do
-  context '#sum' do
-    it 'with positive numbers' do
-        calc = Calculator.new
-        result = calc.sum(5, 7)
-        expect(result).to eql(12)
+  subject(:calc) { described_class.new() }
+
+  context "#sum" do
+    it "with positive numbers" do
+      result = calc.sum(5, 7)
+      expect(result).to eql(12)
     end
 
-    it 'with negative and positive numbers' do
-        calc = Calculator.new
-        result = calc.sum(-5, 7)
-        expect(result).to eql(2)
+    it "with negative and positive numbers" do
+      result = calc.sum(-5, 7)
+      expect(result).to eql(2)
     end
 
-    it 'with negative numbers' do
-        calc = Calculator.new
-        result = calc.sum(-5, -7)
-        expect(result).to eql(-12)
+    it "with negative numbers" do
+      result = calc.sum(-5, -7)
+      expect(result).to eql(-12)
     end
   end
 end
