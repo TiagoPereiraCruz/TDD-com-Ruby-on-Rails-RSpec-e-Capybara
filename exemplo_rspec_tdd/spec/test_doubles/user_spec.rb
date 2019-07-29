dbe90c403917c778f638f03afe3ade08730d0682
Tiago Pereira <tiago.peng@gmail.com>
2019-07-28 23:28:36 -0300

    finalizando test doubles, sutbs e mocks

--- exemplo_rspec_tdd/spec/test_doubles/user_spec.rb
+++ exemplo_rspec_tdd/spec/test_doubles/user_spec.rb
@@ -1,10 +1,20 @@
 describe "Test Double" do
   it "--" do
-    user = double('User')
+    user = double("User")
+    allow(user).to receive_messages(name: "Jack", password: "secret")
+    # allow(user).to receive(:name).and_return('Jack')
+    # allow(user).to receive(:password).and_return('secret')
+    puts user.name
+    puts user.password
+  end
+
+  it "as_null_object" do
+    user = double('User').as_null_object
     allow(user).to receive_messages(name: 'Jack', password: 'secret')
     # allow(user).to receive(:name).and_return('Jack')
     # allow(user).to receive(:password).and_return('secret')
     user.name
     user.password
+    user.abc
   end
 end
